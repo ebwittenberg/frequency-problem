567bc9b3de39950c31facda8e992d1b4904b1cc7
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-02 10:53:21 -0500

    creates file for javascript attempt

---
+++ frequency2.js
@@ -0,0 +1,9 @@
+//  Given two arrays write a function to find out if two arrays have the 
+//  same frequency of digits
+// Examples
+// one = [[1,2,3,4], [1,2,3,4]]
+//  [1,2,3,4], [1,4,5,6] = two
+//  [1,2,3,4], [1,4,4,2] = three
+//  [1,2,3,4], [1,4,3,2] = four
+//  [1,2,3,4,5], [1,2,3,4] = five
+
